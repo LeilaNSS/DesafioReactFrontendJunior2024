6c8a79eea28b18a8005c7d0dec34bc698fce8636
Murbr4 <murillo.ios02@outlook.com>
2024-02-15 21:49:11 -0300

    subida do projeto

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,42 @@
 import React from "react";
+import { useState } from "react";
+import { Item } from './types/Item';
+import * as C from "./app.styles";
+import {ListItem} from './components/ListItem';
+import { AddArea } from './components/AddArea';
 
-export default function App() {
-  return (
-    <section>
-      <h1>Todos</h1>
-    </section>
-  );
+const App = () => {
+    const [list, setList] = useState<Item[]>([
+        {id:1, name: 'comprar bolo', done:false},
+        {id:2, name: 'lavar louça', done:true},
+    ]);
+
+const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+        id: list.length + 1,
+        name: taskName,
+        done: false
+    });
+    setList(newList);
 }
+
+
+    return(
+        <C.Container>
+            <C.Area>
+                    <C.Header>Todos</C.Header>
+
+                    <AddArea onEnter={handleAddTask}        />
+
+                {list.map((item, index)=>(
+                    <ListItem key={index} item={item} /> 
+                             
+                ))}
+
+                </C.Area>
+        </C.Container>
+    );
+}
+
+export default App;
